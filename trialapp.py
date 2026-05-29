c66a415fe6be998c056b7f8f0aa8a99ee99a1a60
nidhivinay271-glitch <nidhivinay271@gmail.com>
2026-05-29 15:18:49 +0530

    Update trialapp.py

--- trialapp.py
+++ trialapp.py
@@ -37,7 +37,7 @@ except Exception:
 VMAX = 5.0
 DEFAULT_FREQUENCY = 1000
 DEFAULT_DUTY_CYCLE = 50
-DEFAULT_TIME_WINDOW = 0.5
+DEFAULT_TIME_WINDOW = 0.05
 
 
 # =============================================================================
