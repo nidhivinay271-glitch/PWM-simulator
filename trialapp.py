b4b59c8b5b323940fb273a8f17894e49f0a0ec41
nidhivinay271-glitch <nidhivinay271@gmail.com>
2026-05-29 18:55:18 +0530

    Update trialapp.py

--- trialapp.py
+++ trialapp.py
@@ -37,7 +37,7 @@ except Exception:
 VMAX = 5.0
 DEFAULT_FREQUENCY = 1000
 DEFAULT_DUTY_CYCLE = 50
-DEFAULT_TIME_WINDOW = 0.5
+DEFAULT_TIME_WINDOW = 0.05
 
 
 # =============================================================================
@@ -790,10 +790,10 @@ graph_mode = st.sidebar.selectbox(
 
 time_window = st.sidebar.slider(
     "Time Window (s)",
-    0.05,
+    0.005,
     2.0,
-    0.1,
-    step=0.05
+    0.05,
+    step=0.005
 )
 pin = st.sidebar.selectbox(
     "PWM Pin",
